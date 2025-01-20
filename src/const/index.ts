d62b0f52df422c4f0faa7d1a825c7a93e8d5d83a
Anav Chand <anavkki@gmail.com>
2025-01-20 18:34:49 +1200

    feat(server): add create blood test result

---
+++ src/const/index.ts
@@ -0,0 +1,9 @@
+import { SubstanceName } from "@prisma/client"
+
+export const substanceNames = [
+    SubstanceName.HEMOGLOBIN,
+    SubstanceName.RED_BLOOD_CELLS,
+    SubstanceName.WHITE_BLOOD_CELLS,
+    SubstanceName.POTASSIUM,
+    SubstanceName.POTASSIUM
+]
---
+++ src/routes/(admin)/dashboard/create/+page.server.ts
@@ -0,0 +1,80 @@
+import type { Substance, SubstanceName } from "@prisma/client"
+import { type Actions, redirect } from "@sveltejs/kit"
+import { db } from "@/prisma"
+
+export const load = async ({ parent }) => {
+    await parent()
+}
+
+export const actions: Actions = {
+    default: async ({ request, locals }) => {
+        if (!locals.user) {
+            redirect(303, "login")
+        }
+
+        const data = await request.formData()
+
+        const date = data.get("date") as string
+        const lab = data.get("lab") as string
+        const comment = data.get("comment") as string || null
+
+        if (!date || !lab) {
+            return {
+                success: false,
+                message: "Some required fields are missing."
+            }
+        }
+
+        const names = data.getAll('name[]') as string[]
+        const values = data.getAll('value[]') as string[]
+        const mins = data.getAll('min[]') as string[]
+        const maxs = data.getAll('max[]') as string[]
+
+        const substances: Substance[] = []
+
+        names.forEach((name, index) => {
+            if (values[index]) {
+                substances.push({
+                    id: crypto.randomUUID(),
+                    name: name as SubstanceName,
+                    value: parseFloat(values[index]),
+                    min: parseFloat(mins[index]),
+                    max: parseFloat(maxs[index]),
+                    testId: "",
+                })
+            }
+        })
+
+        try {
+            await db.$transaction(async (tx) => {
+                const test = await tx.test.create({
+                    data: {
+                        date: new Date(date),
+                        lab,
+                        comment
+                    }
+                })
+
+                await tx.substance.createMany({
+                    data: substances.map((substance) => ({
+                        ...substance,
+                        testId: test.id
+                    }))
+                })
+            })
+
+            return {
+                success: true,
+                message: "Blood test results added successfully."
+            }
+
+        } catch (error) {
+            console.log(error)
+
+            return {
+                success: false,
+                message: "There was an internal server error."
+            }
+        }
+    }
+}
