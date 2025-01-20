8af58613c572b0efaa415e62b16a6868f0de9f3a
Anav Chand <anavkki@gmail.com>
2025-01-20 21:13:01 +1200

    feat: add delete blood test result

---
+++ src/routes/(admin)/dashboard/delete/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    const { data, form } = $props();
+</script>
+
+<h1>Delete result</h1>
+
+<form method="POST">
+    <p>
+        Are you sure you want to delete the blood test result from
+        {data.test?.lab} dated {data.test?.date.toDateString()}?
+    </p>
+    <input type="text" name="id" value={data?.test?.id} hidden />
+    <button>Confirm</button>
+</form>
+
+{#if form?.message}
+    <p class="error">{form?.message}</p>
+{/if}
