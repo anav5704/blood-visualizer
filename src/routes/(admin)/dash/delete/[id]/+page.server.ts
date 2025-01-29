fe9a642a98acf49a8bba3edb37df1f3868917d76
Anav Chand <anavkki@gmail.com>
2025-01-29 19:34:49 +1200

    feat(server): add delete test

---
+++ src/routes/(admin)/dash/delete/[id]/+page.server.ts
@@ -0,0 +1,45 @@
+import { redirect, type Actions } from "@sveltejs/kit"
+import { db } from "@/prisma/index.js"
+
+export const load = async ({ parent, params: { id } }) => {
+    await parent()
+
+    const test = await db.test.findUnique({
+        where: {
+            id
+        }
+    })
+
+    return { test }
+}
+
+export const actions: Actions = {
+    default: async ({ params: { id }, locals }) => {
+        if (!locals.user) {
+            redirect(303, "/login")
+        }
+
+        if (!id) {
+            return {
+                message: "Required fields are missing."
+            }
+        }
+
+        try {
+            await db.test.delete({
+                where: {
+                    id
+                }
+            })
+
+        } catch (error) {
+            console.log(error)
+
+            return {
+                message: "There was an internal server error."
+            }
+        }
+
+        redirect(303, "/dash")
+    }
+}
