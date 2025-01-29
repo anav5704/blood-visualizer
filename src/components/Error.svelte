b9c664f0b9e809b5f2d278154c3539cd0b2f01a1
Anav Chand <anavkki@gmail.com>
2025-01-29 19:30:00 +1200

    feat: add error component

---
+++ src/components/Error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { TriangleAlert } from "lucide-svelte";
+
+    const { message } = $props();
+</script>
+
+<div class="error flex items-center gap-3">
+    <TriangleAlert size={20} />
+    <p>{message}</p>
+</div>
