ff5c05302e601ecd2dd64cf46ab8edef3a83a363
Anav Chand <anavkki@gmail.com>
2025-01-29 19:34:42 +1200

    feat: add delete test

---
+++ src/routes/(admin)/dash/delete/[id]/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Error from "@/components/Error.svelte";
+
+    const { data, form } = $props();
+</script>
+
+<h1>Delete Results</h1>
+
+{#if form?.message}
+    <Error message={form.message} />
+{/if}
+
+<form method="POST">
+    <p>
+        Are you sure you want to delete the blood test result from
+        {data.test?.lab} dated {data.test?.date.toDateString()}?
+    </p>
+    <input type="text" name="id" value={data?.test?.id} hidden />
+    <button>Delete Results</button>
+</form>
