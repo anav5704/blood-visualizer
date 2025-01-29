f9805e393657035c777c3351f3ec07780662df9b
Anav Chand <anavkki@gmail.com>
2025-01-29 19:30:54 +1200

    feat: use nav in global layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Navbar from "@/components/Navbar.svelte";
+    import "@fontsource-variable/dm-sans";
+    import "../app.css";
+
+    let { children, data } = $props();
+</script>
+
+<main>
+    <Navbar user={data.user} />
+    {@render children()}
+</main>
