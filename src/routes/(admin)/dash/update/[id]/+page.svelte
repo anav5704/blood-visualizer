abfcb5033c8772cccc8bd4cd81ab68b7704026ec
Anav Chand <anavkki@gmail.com>
2025-01-29 19:34:07 +1200

    feat: add update test

---
+++ src/routes/(admin)/dash/update/[id]/+page.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+    import Error from "@/components/Error.svelte";
+    import { substanceNames } from "@/const";
+    import { enhance } from "$app/forms";
+    const { data, form } = $props();
+
+    const formattedDate = new Date(data?.test?.date as Date)
+        .toISOString()
+        .split("T")[0];
+</script>
+
+<h1>Update Results</h1>
+
+{#if form?.message}
+    <Error message={form.message} />
+{/if}
+
+<form
+    method="POST"
+    use:enhance={() => {
+        return async ({ update }) => {
+            update({ reset: false });
+        };
+    }}
+>
+    <input type="text" name="id" value={data?.test?.id} hidden />
+    <div class="grid gap-5 grid-cols-2">
+        <input
+            value={formattedDate}
+            placeholder="Date"
+            type="date"
+            name="date"
+            required
+        />
+        <input
+            value={data.test?.lab}
+            placeholder="Lab"
+            type="text"
+            name="lab"
+            required
+        />
+    </div>
+    <textarea placeholder="Comment" name="comment" value={data.test?.comment}
+    ></textarea>
+
+    {#each substanceNames as substanceName}
+        {@const existingSubstance = data?.test?.substances.find(
+            (substance) => substance.name === substanceName
+        )}
+        <div class="grid grid-cols-3 gap-5 items-end">
+            <input type="text" name="name[]" value={substanceName} hidden />
+            <input
+                value={existingSubstance ? existingSubstance.value : ""}
+                placeholder={substanceName}
+                type="number"
+                name="value[]"
+            />
+            <input
+                value={existingSubstance ? existingSubstance.min : ""}
+                placeholder="Min"
+                type="number"
+                name="min[]"
+            />
+            <input
+                value={existingSubstance ? existingSubstance.max : ""}
+                placeholder="Max"
+                type="number"
+                name="max[]"
+            />
+        </div>
+    {/each}
+
+    <button>Update Results</button>
+</form>
