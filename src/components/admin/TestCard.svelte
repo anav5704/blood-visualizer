7c4112a5302ad7e42f90b87074ee245f4a00d994
Anav Chand <anavkki@gmail.com>
2025-01-31 19:25:14 +1200

    feat: add test card

---
+++ src/components/admin/TestCard.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { SquarePen, Trash2 } from "lucide-svelte";
+
+    const { test } = $props();
+</script>
+
+<div class="group card flex justify-between items-center">
+    <div>
+        <p>{test.lab}</p>
+        <p class="text-zinc-500">{test.date.toDateString()}</p>
+    </div>
+    <div class="flex">
+        <a
+            class="transition group-hover:opacity-100 opacity-0 p-3"
+            href={"/dash/update/" + test.id}
+        >
+            <SquarePen size={20} />
+        </a>
+        <a
+            class="transition group-hover:opacity-100 opacity-0 p-3"
+            href={"/dash/delete/" + test.id}
+        >
+            <Trash2 size={20} />
+        </a>
+    </div>
+</div>
