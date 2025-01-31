ee54fab9401d6d418a3ae54cdd4502e24f55f106
Anav Chand <anavkki@gmail.com>
2025-01-31 20:12:02 +1200

    feat: add test/id page

--- src/routes/(guest)/tests/[id]/+page.svelte
+++ src/routes/(guest)/tests/[id]/+page.svelte
@@ -1,1 +1,45 @@
-<h1>Test page</h1>
+<script lang="ts">
+    import { Status } from "@/utils/const/status.js";
+    import { getStatus } from "@/utils/getStatus.js";
+    import type { Substance } from "@prisma/client";
+
+    const { data } = $props();
+
+    const getColor = (substance: Substance) => {
+        const status = getStatus(substance);
+
+        if (status === Status.good) {
+            return "text-emerald-500";
+        }
+
+        if (status === Status.ok) {
+            return "text-amber-500";
+        }
+
+        if (status === Status.bad) {
+            return "text-rose-500";
+        }
+    };
+</script>
+
+<h1>Test Results / {data.test?.date.toDateString()}</h1>
+
+<div class="card space-y-6 p-6">
+    <p>Lab: {data.test?.lab}</p>
+
+    <hr />
+
+    {#each data.test?.substances as substance}
+        <div class="flex justify-between">
+            <p>{substance.name}</p>
+            <div class="flex gap-10">
+                <p class={getColor(substance)}>{substance.value}</p>
+                <p class="text-zinc-500">{substance.min}-{substance.max}</p>
+            </div>
+        </div>
+    {/each}
+
+    <hr />
+
+    <p>Comment: {data.test?.comment}</p>
+</div>
