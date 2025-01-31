5412b86d8ee2e0cb7ac90f27a767ce8b3005efeb
Anav Chand <anavkki@gmail.com>
2025-01-31 19:25:30 +1200

    feat: add status indicator

---
+++ src/components/guest/StatusIndicator.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { Status } from "@/utils/const/status";
+
+    const { status } = $props();
+
+    let background = $state("");
+
+    if (status === Status.good) {
+        background = "bg-emerald-500";
+    }
+
+    if (status === Status.ok) {
+        background = "bg-amber-500";
+    }
+
+    if (status === Status.bad) {
+        background = "bg-rose-500";
+    }
+</script>
+
+<div>
+    <div class={`${background} rounded-full h-5 w-5`}></div>
+</div>
