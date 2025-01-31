315e1f26b7b98da13c69478d4d0840837fb79870
Anav Chand <anavkki@gmail.com>
2025-01-31 19:25:49 +1200

    feat: add all tests page

---
+++ src/routes/(guest)/tests/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import TestCard from "@/components/guest/TestCard.svelte";
+
+    const { data } = $props();
+</script>
+
+<h1>Test Results</h1>
+
+{#each data.tests as test}
+    <TestCard {test} />
+{/each}
