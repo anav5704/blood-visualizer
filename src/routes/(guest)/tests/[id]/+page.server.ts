8128978b363c0d8c1e2245696dcfda8605f93705
Anav Chand <anavkki@gmail.com>
2025-01-31 20:12:10 +1200

    feat(server): add test/id page

---
+++ src/routes/(guest)/tests/[id]/+page.server.ts
@@ -0,0 +1,19 @@
+import { db } from "@/prisma"
+import { error } from "@sveltejs/kit"
+
+export const load = async ({ params: { id } }) => {
+    const test = await db.test.findUnique({
+        where: {
+            id
+        },
+        include: {
+            substances: true
+        }
+    })
+
+    if (!test) {
+        error(404, "Test not found")
+    }
+
+    return { test }
+}
