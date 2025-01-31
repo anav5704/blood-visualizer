c9938cb09c638f0068d45f0ede1c940f215f411f
Anav Chand <anavkki@gmail.com>
2025-01-31 19:26:03 +1200

    feat: add all substances page

---
+++ src/routes/(guest)/substances/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import SubstanceCard from "@/components/guest/SubstanceCard.svelte";
+    import { substanceNames } from "@/const";
+
+    const { data } = $props();
+</script>
+
+<h1>Substances</h1>
+
+{#each data.groupedSubstances as substance}
+    <SubstanceCard {substance} />
+{/each}
