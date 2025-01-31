8cf58fdc6c5d06464142eebac6a896605a4f17ad
Anav Chand <anavkki@gmail.com>
2025-01-31 19:25:19 +1200

    feat: add test card

---
+++ src/components/guest/TestCard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import StatusIndicator from "./StatusIndicator.svelte";
+
+    const { test } = $props();
+</script>
+
+<a
+    class="group card flex justify-between items-center"
+    href={"/tests/" + test.id}
+>
+    <div>
+        <p>{test.lab}</p>
+        <p class="text-zinc-500">{test.date.toDateString()}</p>
+    </div>
+    <StatusIndicator status={test.status} />
+</a>
