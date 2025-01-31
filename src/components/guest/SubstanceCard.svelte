327e89d1e3052b0b6e24d14a205761e80e7ec8dc
Anav Chand <anavkki@gmail.com>
2025-01-31 19:25:24 +1200

    feat: add substance card

---
+++ src/components/guest/SubstanceCard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import StatusIndicator from "./StatusIndicator.svelte";
+
+    const { substance } = $props();
+</script>
+
+<a
+    class="group card flex justify-between items-center"
+    href={"/substances/" + substance.name}
+>
+    <div>
+        <p>{substance.name}</p>
+        <p class="text-zinc-500">Full blood count</p>
+    </div>
+    <StatusIndicator status={substance.status} />
+</a>
