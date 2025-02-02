7c28185c1ce83a1ee9235ced8f59b81884788532
Anav Chand <anavkki@gmail.com>
2025-02-03 11:49:45 +1200

    chore: refine test page

--- src/routes/(guest)/tests/[id]/+page.svelte
+++ src/routes/(guest)/tests/[id]/+page.svelte
@@ -1,45 +1,10 @@
 <script lang="ts">
-    import { getSingularStatus } from "@/utils/getSingularStatus";
-    import type { Substance } from "@prisma/client";
-    import { Status } from "@/utils/const";
+    import TestTable from "@/components/guest/TestTable.svelte";
+    import { getTextColor } from "@/utils/getTextColor.js";
 
     const { data } = $props();
-
-    const getColor = (substance: Substance) => {
-        const status = getSingularStatus(substance);
-
-        if (status === Status.good) {
-            return "text-emerald-500";
-        }
-
-        if (status === Status.ok) {
-            return "text-amber-500";
-        }
-
-        if (status === Status.bad) {
-            return "text-rose-500";
-        }
-    };
 </script>
 
 <h1>Test Results / {data.test?.date.toDateString()}</h1>
 
-<div class="card space-y-6 p-6">
-    <p>Lab: {data.test?.lab}</p>
-
-    <hr />
-
-    {#each data.test?.substances as substance}
-        <div class="flex justify-between">
-            <p>{substance.name}</p>
-            <div class="flex gap-10">
-                <p class={getColor(substance)}>{substance.value}</p>
-                <p class="text-zinc-500">{substance.min}-{substance.max}</p>
-            </div>
-        </div>
-    {/each}
-
-    <hr />
-
-    <p>Comment: {data.test?.comment}</p>
-</div>
+<TestTable test={data.test} />
