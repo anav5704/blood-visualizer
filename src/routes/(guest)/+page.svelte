ef76b94a90c0fb99d8ec06d8abd7ede1a5a62c0c
Anav Chand <anavkki@gmail.com>
2025-02-03 11:49:54 +1200

    feat: add overview page

--- src/routes/(guest)/+page.svelte
+++ src/routes/(guest)/+page.svelte
@@ -1,1 +1,16 @@
-<h1>Home page</h1>
+<script lang="ts">
+    import CountCard from "@/components/guest/CountCard.svelte";
+    import TestTable from "@/components/guest/TestTable.svelte";
+
+    const { data } = $props();
+</script>
+
+<h1>Overview</h1>
+
+<div class="grid grid-cols-3 gap-5">
+    <CountCard label="Total Tests" value={data.count.test} />
+    <CountCard label="Total Substances" value={data.count.substance} />
+    <CountCard label="Healthy Results" value={data.count.healthy + "%"} />
+</div>
+
+<TestTable test={data.test} />
