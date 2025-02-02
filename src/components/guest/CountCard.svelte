06f8712f48805abd418445aed6d01fa783e2e25c
Anav Chand <anavkki@gmail.com>
2025-02-03 11:47:52 +1200

    feat: add count card

---
+++ src/components/guest/CountCard.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    const { label, value } = $props();
+</script>
+
+<div class="card">
+    <p class="faded">{label}</p>
+    <p class="font-semibold text-3xl text-right">{value}</p>
+</div>
