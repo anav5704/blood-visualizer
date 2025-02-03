03e418bb913267583a112117e0ed9bf576cfda26
Anav Chand <anavkki@gmail.com>
2025-02-03 15:14:10 +1200

    feat: add list loader

---
+++ src/components/guest/ListLoader.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+</script>
+
+{#each items as item}
+    <div class="card animate-pulse">
+        <p class="opacity-0">Loading...</p>
+        <p class="opacity-0">Loading...</p>
+    </div>
+{/each}
