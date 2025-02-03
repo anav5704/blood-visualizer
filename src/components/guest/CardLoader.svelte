9f38cc9f11ca9c40fc038be8ff454ca3474ce134
Anav Chand <anavkki@gmail.com>
2025-02-03 12:23:03 +1200

    feat: add card loader

---
+++ src/components/guest/CardLoader.svelte
@@ -0,0 +1,14 @@
+<div class="grid grid-cols-3 gap-5">
+    <div class="card animate-pulse">
+        <p class="opacity-0">e</p>
+        <p class="opacity-0 text-3xl">e</p>
+    </div>
+    <div class="card animate-pulse">
+        <p class="opacity-0">e</p>
+        <p class="opacity-0 text-3xl">e</p>
+    </div>
+    <div class="card animate-pulse">
+        <p class="opacity-0">e</p>
+        <p class="opacity-0 text-3xl">e</p>
+    </div>
+</div>
