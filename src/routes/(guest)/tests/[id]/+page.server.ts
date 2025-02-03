baa92f036a6180d002046af161842dba9430294a
Anav Chand <anavkki@gmail.com>
2025-02-03 15:14:55 +1200

    feat: add skeleton loading to tests/id page

--- src/routes/(guest)/tests/[id]/+page.server.ts
+++ src/routes/(guest)/tests/[id]/+page.server.ts
@@ -1,20 +1,18 @@
-import { Substances } from "@/utils/const/index.js"
-import { error } from "@sveltejs/kit"
 import { db } from "@/prisma"
 
 export const load = async ({ params: { id } }) => {
-    let test = await db.test.findUnique({
-        where: {
-            id
-        },
-        include: {
-            substances: true
-        }
-    })
+    const stream = async () => {
+        let test = await db.test.findUnique({
+            where: {
+                id
+            },
+            include: {
+                substances: true
+            }
+        })
 
-    if (!test) {
-        error(404, "Test not found")
+        return { test }
     }
 
-    return { test }
+    return { stream: stream() }
 }
--- src/routes/(guest)/tests/[id]/+page.svelte
+++ src/routes/(guest)/tests/[id]/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+    import TableLoader from "@/components/guest/TableLoader.svelte";
     import TestTable from "@/components/guest/TestTable.svelte";
-    import { getTextColor } from "@/utils/getTextColor.js";
 
     const { data } = $props();
 </script>
 
-<h1>Test Results / {data.test?.date.toDateString()}</h1>
-
-<TestTable test={data.test} />
+{#await data.stream}
+    <h1>Test Results</h1>
+    <TableLoader />
+{:then stream}
+    <h1>Test Results / {stream.test?.date.toDateString()}</h1>
+    <TestTable test={stream.test} />
+{/await}
